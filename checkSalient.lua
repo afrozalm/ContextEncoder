bf94715591b8b40fd8123a8de4887287f9976212
afrozalm <alam.afroz011@gmail.com>
2016-08-29 12:22:44 +0530

    Corrected ranking code

--- checkSalient.lua
+++ checkSalient.lua
@@ -14,14 +14,13 @@ cutorch.synchronize()
 
 testset = torch.load('../FaceScrub/FaceScrub_testset_128x128')
 trainset = torch.load('../FaceScrub/FaceScrub_trainset_128x128')
-generator = torch.load('./generator.net')
-discriminator = torch.load('./discriminator.net')
+generator = torch.load('./generatorFace.net')
+discriminator = torch.load('./discriminatorFace.net')
 
 salientTest = torch.Tensor( params['max'] ):zero()
 salientTrain = torch.Tensor( params['max'] ):zero()
-current_size = 0
 
-lamda_rec = 0.50
+lamda_rec = 0.30
 lamda_adv = 1 - lamda_rec
 
 criterion_rec = nn.MSECriterion():cuda()
@@ -87,44 +86,7 @@ function fillArr( a, tree )
 end
 
 root = nil
-for i = 1, testset.size do
-    collectgarbage()
-    targetImage[1] = testset.data[{{ i }, {}, {33, 96}, {33, 96}}]
-    targetImage[2] = targetImage[1]
-    maskedInput[1] = testset.data[i]
-    maskedInput[2] = maskedInput[1]
-    targetLabel = torch.CudaTensor( 2 ):fill(1)
-    maskedInput:cuda()
-    targetImage:cuda()
-    maskedInput:cmul(M)
-    outputImage = generator:forward( maskedInput )
-    print( i )
-    err_rec = criterion_rec:forward( outputImage, targetImage )
-    err_adv = criterion_adv:forward( discriminator:forward( stitch( outputImage, maskedInput ) ), targetLabel )
-    err = err_rec * lamda_rec + err_adv * lamda_adv
-    if current_size == 0 then
-        current_size = 1
-        root = insert( err, root, i )
-    elseif current_size == params['max'] then
-        root = removeMin( root )
-        root = insert( err, root, i )
-    else
-        root = insert( err, root, i )
-        current_size = current_size + 1
-    end
-end
-
-current_index = 1
-fillArr( salientTest, root )
-
-i = params['max']
-while i > 0 do
-    name = i .. '_rankedTestSalient.jpg'
-    image.save( name, testset.data[salientTest[i]] )
-    i = i - 1
-end
-
-root = nil
+current_size = 0
 for i = 1, trainset.size do
     collectgarbage()
     targetImage[1] = trainset.data[{{ i }, {}, {33, 96}, {33, 96}}]
@@ -157,7 +119,46 @@ fillArr( salientTrain, root )
 
 i = params['max']
 while i > 0 do
-    name = i .. '_rankedTrainSalient.jpg'
+    name = params['max'] + 1 - i .. '_point_3_rankedTrainSalient.jpg'
     image.save( name, trainset.data[salientTrain[i]] )
+    i = i - 1
+end
+
+root = nil
+current_size = 0
+for i = 1, testset.size do
+    collectgarbage()
+    targetImage[1] = testset.data[{{ i }, {}, {33, 96}, {33, 96}}]
+    targetImage[2] = targetImage[1]
+    maskedInput[1] = testset.data[i]
+    maskedInput[2] = maskedInput[1]
+    targetLabel = torch.CudaTensor( 2 ):fill(1)
+    maskedInput:cuda()
+    targetImage:cuda()
+    maskedInput:cmul(M)
+    outputImage = generator:forward( maskedInput )
+    print( i )
+    err_rec = criterion_rec:forward( outputImage, targetImage )
+    err_adv = criterion_adv:forward( discriminator:forward( stitch( outputImage, maskedInput ) ), targetLabel )
+    err = err_rec * lamda_rec + err_adv * lamda_adv
+    if current_size == 0 then
+        current_size = 1
+        root = insert( err, root, i )
+    elseif current_size == params['max'] then
+        root = removeMin( root )
+        root = insert( err, root, i )
+    else
+        root = insert( err, root, i )
+        current_size = current_size + 1
+    end
+end
+
+current_index = 1
+fillArr( salientTest, root )
+
+i = params['max']
+while i > 0 do
+    name = params['max'] - i + 1 .. '_point_3_rankedTestSalient.jpg'
+    image.save( name, testset.data[salientTest[i]] )
     i = i - 1
 end
